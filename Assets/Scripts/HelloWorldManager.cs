32dda76dbac10c2cf78c8e448b34b081987fc5ce
Víctor Díaz Fernández <diafervic@IESPIRINGALLA.AD>
2023-05-16 20:07:03 +0200

    Funcionalidade networktransform acadada

---
+++ Assets/Scripts/HelloWorldManager.cs
@@ -0,0 +1,67 @@
+using Unity.Netcode;
+using UnityEngine;
+
+namespace HelloWorld
+{
+    public class HelloWorldManager : NetworkBehaviour
+    {
+
+        public NetworkList<int> usedColors;
+
+        void Awake() {
+            usedColors = new NetworkList<int>();
+        }
+
+        void OnGUI()
+        {
+            GUILayout.BeginArea(new Rect(10, 10, 300, 300));
+            if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
+            {
+                StartButtons();
+            }
+            else
+            {
+                StatusLabels();
+
+                SubmitChangeColor();
+            }
+
+            GUILayout.EndArea();
+        }
+
+        static void StartButtons()
+        {
+            if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
+            if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
+            if (GUILayout.Button("Server")) NetworkManager.Singleton.StartServer();
+        }
+
+        static void StatusLabels()
+        {
+            var mode = NetworkManager.Singleton.IsHost ?
+                "Host" : NetworkManager.Singleton.IsServer ? "Server" : "Client";
+
+            GUILayout.Label("Transport: " +
+                NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
+            GUILayout.Label("Mode: " + mode);
+        }
+
+        static void SubmitChangeColor()
+        {
+            if (GUILayout.Button("Change color"))
+            {
+                var playerObject = NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject();
+                var player = playerObject.GetComponent<HelloWorldPlayer>();
+                player.ChangeColor();
+            }
+        }
+        
+        public void AddColor(int color) {
+            usedColors.Add(color);
+        }
+
+        public void RemoveColor(int color) {
+            usedColors.Remove(color);
+        }
+    }
+}
